73ba776392ccc92048c2dc94a0020da68f92f0af
Sebastian Guerra <spguerraespinoza@gmail.com>
2024-06-27 20:10:05 -0400

    ✨ add provider to make queries

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import ContextsProvider from './models/contexts_provider';
 import RouterBuilder from './controllers/routes_builder';
+import { QueryClientProvider } from '@tanstack/react-query';
+
+import queryClient from './query_client';
 
 const App = () => {
   return (
-    <ContextsProvider>
-      <RouterBuilder />
-    </ContextsProvider>
+    <QueryClientProvider client={queryClient}>
+      <ContextsProvider>
+        <RouterBuilder />
+      </ContextsProvider>
+    </QueryClientProvider>
   );
 };
 

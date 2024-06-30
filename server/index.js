96ff1628d6c954657228ede2f3b4d67d2f3320ad
Sebastian Guerra <spguerraespinoza@gmail.com>
2024-06-30 17:27:05 -0400

    🤡 mock paths logic

--- server/index.js
+++ server/index.js
@@ -1,17 +1,50 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
-const port = process.env.PORT ?? 8000;
+app.use(cors());
+const port = process.env.PORT ?? 3000;
+
+const users = {};
 
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-app.post('/register', () => {});
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
 
-app.post('/login', () => {});
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+  if (users[username]) {
+    res.status(400).send('User already exists');
+  } else {
+    users[username] = password;
+    res.send('User registered');
+  }
+});
 
-app.get('/me', () => {});
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  if (users[username] === password) {
+    res.send('Login successful');
+  } else {
+    res.status(401).send('Login failed');
+  }
+});
+
+app.get('/me', (req, res) => {
+  const token = req.headers.authorization;
+  console.log(token);
+  if (!token) {
+    res.status(401).send('Unauthorized');
+    return;
+  }
+  res.json({
+    username: 'asdf',
+  })
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);

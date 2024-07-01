452f3177db0eb01211c60e4669d19cf9c564e8f6
Sebastian Guerra <spguerraespinoza@gmail.com>
2024-07-01 02:16:25 -0400

    ♻️ refactor

--- client/src/views/PanelGroup.tsx
+++ client/src/views/PanelGroup.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
@@ -51,7 +50,7 @@ const PanelGroup: React.FC<PanelGroupProps> = ({ columns, rows }) => {
 
   return (
     <DropInput
-      onDrop={handleFileChange}
+      onDrop={(files) => void handleFileChange(files)}
       borderColor="black"
       onDragOverColor="blue"
       style={{

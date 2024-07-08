ac3033558a2b170e128dfe2e145a688d58ddce79
Sebastian Guerra <spguerraespinoza@gmail.com>
2024-07-07 22:11:52 -0400

    ✨ allow to select image to see

--- client/src/views/PanelGroup.tsx
+++ client/src/views/PanelGroup.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect } from 'react';
 
+import Select from 'react-select';
 import cornerstone from 'cornerstone-core';
 import dicomParser from 'dicom-parser';
 import cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
@@ -21,14 +22,13 @@ const PanelGroup: React.FC<PanelGroupProps> = ({ columns, rows }) => {
     cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
   }, []);
 
-  const [imgs, setImgs] = React.useState<string[][]>([]);
-  const cleanList = useMemo(() => {
-    return (
-      imgs
-        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-        .map((stack) => stack?.filter((x) => x))
-    );
-  }, [imgs]);
+  const [selectedImageSet, setSelectedImageSet] = React.useState<number | null>(null);
+
+  const [imgSets, setImgSets] = React.useState<string[][]>([]);
+
+  const [imgs, setImgs] = React.useState<
+    Record<number, { imageId: string; instanceNumber: number }[]>
+  >({});
 
   const handleFileChange = async (files: FileList) => {
     for (const file of files) {
@@ -37,56 +37,105 @@ const PanelGroup: React.FC<PanelGroupProps> = ({ columns, rows }) => {
         .split('-')
         .map((s) => parseInt(s, 10));
 
+      if (!imgSets[imgSet]) {
+        setImgSets((prev) => {
+          const newImgSets = [...prev];
+          newImgSets[imgSet] = [];
+          return newImgSets;
+        });
+      }
+
       const imageId: string = cornerstoneWADOImageLoader.wadouri.fileManager.add(file);
 
       setImgs((prev) => {
-        const newImgs = [...prev];
-        if (!newImgs[imgSet]) newImgs[imgSet] = [];
-        newImgs[imgSet][imgId] = imageId;
-        return newImgs;
+        if (!(imgSet in prev)) {
+          return {
+            ...prev,
+            [imgSet]: [{ imageId, instanceNumber: imgId }],
+          };
+        } else {
+          return {
+            ...prev,
+            [imgSet]: [...prev[imgSet], { imageId, instanceNumber: imgId }],
+          };
+        }
       });
+      setSelectedImageSet(imgSet);
     }
   };
 
   return (
-    <DropInput
-      key={columns * rows}
-      onDrop={(files) => void handleFileChange(files)}
-      borderColor="black"
-      onDragOverColor="blue"
+    <div
       style={{
         height: '100%',
         width: '100%',
         display: 'flex',
-        flexDirection: 'row',
+        flexDirection: 'column',
       }}
     >
-      <div
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <Select
+          styles={{
+            container: (provided) => ({
+              ...provided,
+              width: '80%',
+            }),
+          }}
+          options={Object.keys(imgSets)
+            .filter((key) => imgSets[parseInt(key, 10)])
+            .map((key) => ({
+              value: parseInt(key, 10),
+              label: key,
+            }))}
+          onChange={(selected) => {
+            setSelectedImageSet(selected?.value ?? null);
+          }}
+        />
+        <input
+          type="file"
+          multiple
+          onChange={(e) => e.target.files && void handleFileChange(e.target.files)}
+        />
+      </div>
+      <DropInput
+        key={columns * rows}
+        onDrop={(files) => void handleFileChange(files)}
+        borderColor="black"
+        onDragOverColor="blue"
         style={{
-          display: 'grid',
+          height: '90%',
           width: '100%',
-          height: '100%',
-          backgroundColor: 'black',
-          gridTemplateColumns: `repeat(${columns.toString()}, 1fr)`,
-          gridTemplateRows: `repeat(${rows.toString()}, 1fr)`,
+          display: 'flex',
+          flexDirection: 'row',
         }}
       >
-        {Array.from({ length: columns * rows }).map((_, i) => (
-          <Panel
-            key={i}
-            imageIds={cleanList[i] ?? []}
-            style={{
-              width: '100%',
-              height: '100%',
-              backgroundColor: 'darkgray',
-              borderWidth: '1px',
-              borderStyle: 'solid',
-              borderRadius: '5px',
-            }}
-          />
-        ))}
-      </div>
-    </DropInput>
+        <div
+          style={{
+            display: 'grid',
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'black',
+            gridTemplateColumns: `repeat(${columns.toString()}, 1fr)`,
+            gridTemplateRows: `repeat(${rows.toString()}, 1fr)`,
+          }}
+        >
+          {Array.from({ length: columns * rows }).map((_, i) => (
+            <Panel
+              key={i}
+              imageIds={selectedImageSet ? imgs[selectedImageSet].map((img) => img.imageId) : []}
+              style={{
+                width: '100%',
+                height: '100%',
+                backgroundColor: 'darkgray',
+                borderWidth: '1px',
+                borderStyle: 'solid',
+                borderRadius: '5px',
+              }}
+            />
+          ))}
+        </div>
+      </DropInput>
+    </div>
   );
 };
 

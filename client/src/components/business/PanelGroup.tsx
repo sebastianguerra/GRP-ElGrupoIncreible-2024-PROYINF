108520778175689c846e78c5f2cedc51d9b3dd67
Sebastian Guerra <spguerraespinoza@gmail.com>
2024-10-14 01:47:32 -0300

    ♻️ Move buttons to upload files to DropInput component

--- client/src/components/business/PanelGroup.tsx
+++ client/src/components/business/PanelGroup.tsx
@@ -1,7 +1,5 @@
-import { Grid, GridItem, HStack, IconButton, Tooltip, VStack } from '@chakra-ui/react';
-import { AnimatePresence, motion } from 'framer-motion';
-import React, { useEffect, useState } from 'react';
-import { FiFile, FiFolder, FiUpload } from 'react-icons/fi';
+import { Grid, GridItem, HStack } from '@chakra-ui/react';
+import { useState } from 'react';
 
 import DropInput from '@/components/ui/DropInput';
 import { Layout } from '@/components/ui/LayoutSelector';
@@ -21,135 +19,41 @@ function PanelGroup({ layout }: PanelGroupProps) {
     setImageIds(await addFiles(files));
   };
 
-  const fileInputRef = React.useRef<HTMLInputElement>(null);
-
-  const folderInputRef = React.useRef<HTMLInputElement>(null);
-  useEffect(() => {
-    if (folderInputRef.current) {
-      // Adds the ability to select folders in the file input
-      folderInputRef.current.setAttribute('directory', '');
-      folderInputRef.current.setAttribute('webkitdirectory', '');
-      folderInputRef.current.setAttribute('mozdirectory', '');
-    }
-  }, [folderInputRef]);
-
   const [columns, rows] = layout;
 
-  const [buttonsHover, setButtonsHover] = useState(false);
-
   return (
-    <>
-      <DropInput
-        key={`${columns}x${rows}`}
-        onDrop={(files) => void handleFileChange(Array.from(files))}
-        borderColor="black"
-        onDragOverColor="blue"
-        h="full"
+    <DropInput
+      key={`${columns}x${rows}`}
+      onDrop={(files) => void handleFileChange(Array.from(files))}
+      borderColor="black"
+      onDragOverColor="blue"
+      h="full"
+      w="full"
+      as={HStack}
+    >
+      <Grid
         w="full"
-        as={HStack}
+        h="full"
+        bgColor="black"
+        templateColumns={`repeat(${columns}, 1fr)`}
+        templateRows={`repeat(${rows}, 1fr)`}
+        key={`${columns}x${rows}`}
       >
-        <Grid
-          w="full"
-          h="full"
-          bgColor="black"
-          templateColumns={`repeat(${columns}, 1fr)`}
-          templateRows={`repeat(${rows}, 1fr)`}
-          key={`${columns}x${rows}`}
-        >
-          {Array.from({ length: columns * rows }).map((_, i) => (
-            <GridItem key={`panel-${i}-group-${columns}x${rows}`} w="full" h="full">
-              <Panel
-                imageIds={imageIds}
-                w="full"
-                h="full"
-                bgColor="darkgray"
-                borderWidth="1px"
-                borderStyle="solid"
-                borderRadius="5px"
-              />
-            </GridItem>
-          ))}
-        </Grid>
-      </DropInput>
-      <input
-        ref={fileInputRef}
-        type="file"
-        multiple
-        onChange={(e) => e.target.files && void handleFileChange(Array.from(e.target.files))}
-        style={{ display: 'none' }}
-      />
-      <input
-        ref={folderInputRef}
-        type="file"
-        multiple
-        onChange={(e) => {
-          if (e.target.files) {
-            console.log(Array.from(e.target.files));
-            void handleFileChange(Array.from(e.target.files));
-          }
-        }}
-        style={{ display: 'none' }}
-      />
-
-      <VStack
-        position="absolute"
-        bottom="30px"
-        right="30px"
-        onMouseEnter={() => {
-          setTimeout(() => {
-            setButtonsHover(true);
-          }, 100);
-        }}
-        onMouseLeave={() => {
-          setTimeout(() => {
-            setButtonsHover(false);
-          }, 200);
-        }}
-      >
-        <AnimatePresence>
-          {buttonsHover && (
-            <Tooltip
-              label="Subir carpeta"
-              aria-label="Subir carpeta"
-              hasArrow
-              placement="left"
-              shouldWrapChildren
-            >
-              <IconButton
-                key="panel-group-folder-input"
-                borderRadius="full"
-                icon={<FiFolder />}
-                aria-label="Subir archivos"
-                onClick={() => folderInputRef.current?.click()}
-                colorScheme="cyan"
-                as={motion.button}
-                initial={{ translateY: '40px', opacity: 0 }}
-                animate={{ translateY: '0', opacity: 1 }}
-                exit={{ translateY: '40px', opacity: 0 }}
-                transition="all 0.05s ease"
-              />
-            </Tooltip>
-          )}
-        </AnimatePresence>
-        <Tooltip
-          label="Subir archivos"
-          aria-label="Subir archivos"
-          hasArrow
-          placement="left"
-          shouldWrapChildren
-        >
-          <IconButton
-            borderRadius="full"
-            icon={buttonsHover ? <FiFile /> : <FiUpload />}
-            aria-label="Subir archivos"
-            onClick={() => fileInputRef.current?.click()}
-            colorScheme="cyan"
-            transform={buttonsHover ? 'rotate(360deg)' : 'none'}
-            transition="all 0.25s ease"
-          />
-        </Tooltip>
-      </VStack>
-    </>
+        {Array.from({ length: columns * rows }).map((_, i) => (
+          <GridItem key={`panel-${i}-group-${columns}x${rows}`} w="full" h="full">
+            <Panel
+              imageIds={imageIds}
+              w="full"
+              h="full"
+              bgColor="darkgray"
+              borderWidth="1px"
+              borderStyle="solid"
+              borderRadius="5px"
+            />
+          </GridItem>
+        ))}
+      </Grid>
+    </DropInput>
   );
 }
 
--- client/src/components/ui/DropInput.tsx
+++ client/src/components/ui/DropInput.tsx
@@ -1,5 +1,7 @@
-import { Box, BoxProps } from '@chakra-ui/react';
-import React, { useEffect, useImperativeHandle, useRef, useState } from 'react';
+import { Box, BoxProps, IconButton, Tooltip, VStack } from '@chakra-ui/react';
+import { AnimatePresence, motion } from 'framer-motion';
+import React, { useCallback, useEffect, useImperativeHandle, useRef, useState } from 'react';
+import { FiFile, FiFolder, FiUpload } from 'react-icons/fi';
 
 interface DropInputProps extends Omit<BoxProps, 'onDrop'> {
   onDragOverColor?: string;
@@ -11,12 +13,23 @@ const DropInput = React.forwardRef(function DropInput(
   props: DropInputProps,
   forwardedRef: React.Ref<HTMLDivElement | null>,
 ) {
-  const { children, onDrop, onDragOverColor, borderColor: propsBorderColor, ...rest } = props;
+  const {
+    children,
+    onDrop: onDropProp,
+    onDragOverColor,
+    borderColor: propsBorderColor,
+    ...rest
+  } = props;
 
-  const ref = useRef<HTMLDivElement | null>(null);
-  useImperativeHandle(forwardedRef, () => ref.current);
+  const onDrop = useCallback(
+    (files: FileList | undefined | null) => {
+      if (files) onDropProp(files);
+    },
+    [onDropProp],
+  );
 
-  const element: HTMLDivElement | null = ref.current;
+  const dropInputRef = useRef<HTMLDivElement | null>(null);
+  useImperativeHandle(forwardedRef, () => dropInputRef.current);
 
   const [isDragOver, setIsDragOver] = useState(false);
   const [borderColor, setBorderColor] = useState(propsBorderColor);
@@ -26,43 +39,137 @@ const DropInput = React.forwardRef(function DropInput(
   }, [propsBorderColor, onDragOverColor, isDragOver]);
 
   useEffect(() => {
-    if (element) {
-      element.addEventListener('dragover', (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        setIsDragOver(true);
-      });
+    const element = dropInputRef.current;
+    if (!element) return () => {};
 
-      element.addEventListener('dragleave', (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        setIsDragOver(false);
-      });
+    element.addEventListener('dragover', (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      setIsDragOver(true);
+    });
 
-      element.addEventListener('drop', (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        setIsDragOver(false);
+    element.addEventListener('dragleave', (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      setIsDragOver(false);
+    });
 
-        if (e.dataTransfer?.files) {
-          onDrop(e.dataTransfer.files);
-        }
-      });
-    }
+    element.addEventListener('drop', (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      setIsDragOver(false);
+
+      onDrop(e.dataTransfer?.files);
+    });
 
     return () => {
-      if (element) {
-        element.removeEventListener('dragover', () => {});
-        element.removeEventListener('dragleave', () => {});
-        element.removeEventListener('drop', () => {});
-      }
+      element.removeEventListener('dragover', () => {});
+      element.removeEventListener('dragleave', () => {});
+      element.removeEventListener('drop', () => {});
     };
-  }, [element, onDrop]);
+  }, [dropInputRef, onDrop]);
+
+  const fileInputRef = React.useRef<HTMLInputElement>(null);
+
+  const folderInputRef = React.useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    if (folderInputRef.current) {
+      // Adds the ability to select folders in the file input
+      folderInputRef.current.setAttribute('directory', '');
+      folderInputRef.current.setAttribute('webkitdirectory', '');
+      folderInputRef.current.setAttribute('mozdirectory', '');
+    }
+  }, [folderInputRef]);
+
+  const clickInput = (inputRef: React.RefObject<HTMLInputElement>) => {
+    return inputRef.current?.click();
+  };
+  const [buttonsHover, setButtonsHover] = useState(false);
 
   return (
-    <Box ref={ref} borderWidth="3px" borderStyle="solid" {...rest} borderColor={borderColor}>
-      {children}
-    </Box>
+    <>
+      <Box
+        ref={dropInputRef}
+        borderWidth="3px"
+        borderStyle="solid"
+        {...rest}
+        borderColor={borderColor}
+      >
+        {children}
+      </Box>
+      {[fileInputRef, folderInputRef].map((inputRef, i) => (
+        <input
+          key={`panel-group-input-${i}`}
+          ref={inputRef}
+          type="file"
+          multiple
+          onChange={(e) => {
+            onDrop(e.target.files);
+          }}
+          style={{ display: 'none' }}
+        />
+      ))}
+
+      <VStack
+        position="absolute"
+        bottom="30px"
+        right="30px"
+        onMouseEnter={() => {
+          setTimeout(() => {
+            setButtonsHover(true);
+          }, 100);
+        }}
+        onMouseLeave={() => {
+          setTimeout(() => {
+            setButtonsHover(false);
+          }, 200);
+        }}
+      >
+        <AnimatePresence>
+          {buttonsHover && (
+            <Tooltip
+              label="Subir carpeta"
+              aria-label="Subir carpeta"
+              hasArrow
+              placement="left"
+              shouldWrapChildren
+            >
+              <IconButton
+                key="panel-group-folder-input"
+                borderRadius="full"
+                icon={<FiFolder />}
+                aria-label="Subir archivos"
+                onClick={() => clickInput(folderInputRef)}
+                colorScheme="cyan"
+                as={motion.button}
+                initial={{ translateY: '40px', opacity: 0 }}
+                animate={{ translateY: '0', opacity: 1 }}
+                exit={{ translateY: '40px', opacity: 0 }}
+                transition="all 0.05s ease"
+              />
+            </Tooltip>
+          )}
+        </AnimatePresence>
+        <Tooltip
+          label="Subir archivos"
+          aria-label="Subir archivos"
+          hasArrow
+          placement="left"
+          shouldWrapChildren
+        >
+          <IconButton
+            borderRadius="full"
+            aria-label="Subir archivos"
+            onClick={() => clickInput(fileInputRef)}
+            colorScheme="cyan"
+            transition="all 0.25s ease"
+            {...(buttonsHover
+              ? { icon: <FiFile />, transform: 'rotate(360deg)' }
+              : { icon: <FiUpload />, transform: 'none' })}
+          />
+        </Tooltip>
+      </VStack>
+    </>
   );
 });
 
